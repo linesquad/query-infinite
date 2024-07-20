e2f32ea36f3209d0db51dd63b493220fa31c5c02
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-07-20 18:18:44 +0400

    implement infinite query

--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,5 +1,7 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+// we need to import infinite query
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -8,6 +10,17 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
+  // here we using use infinite and getting:
+  // data as pages, fetchNextPage to move on next page and has next page as boolean to check
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    // we need query key
+    queryKey: ["sw-people"],
+    // in query function we get prop page param and we can set as default which will fetch
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    // with this get next param it will move from last page and if last page doesnt exist then undefined
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
   return <InfiniteScroll />;
 }
